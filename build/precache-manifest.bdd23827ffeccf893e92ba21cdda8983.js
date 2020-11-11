f37e8dc7e10eb5cdf3d63a6ba005185ef72565bb
Dave Regg <dave.r.regg@gmail.com>
2020-11-11 15:26:36 -0500

    Prepping

---
+++ build/precache-manifest.bdd23827ffeccf893e92ba21cdda8983.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35ed59c6f1a59e8635fdad295eeb19d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7b7e6154909e59f6b7b",
+    "url": "/static/css/2.d00bb1db.chunk.css"
+  },
+  {
+    "revision": "55144f9786a713670926",
+    "url": "/static/css/main.68ce5fe8.chunk.css"
+  },
+  {
+    "revision": "b7b7e6154909e59f6b7b",
+    "url": "/static/js/2.cc72096c.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.cc72096c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55144f9786a713670926",
+    "url": "/static/js/main.a506ef05.chunk.js"
+  },
+  {
+    "revision": "57db57a8362aff96fbf7",
+    "url": "/static/js/runtime-main.36200b1d.js"
+  }
+]);
